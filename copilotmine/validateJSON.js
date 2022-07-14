cc4b40366e4317736f469ba47b9f707889822a1c
Brittany Reid <39616756+Brittany-Reid@users.noreply.github.com>
2022-07-14 14:45:53 +0930

    reran for empty

---
+++ copilotmine/validateJSON.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+console.log("Parsing file to check formatting")
+var file = fs.readFileSync("data/snippets.json", "utf-8")
+var data = JSON.parse(file);
+var snippets = undefined;
+console.log(`Checking data. 
+SAME snippets indicate that a previous query might have been recorded again accidentally.
+This might occur if the code not able to be updated to a new query. 
+EMPTY indicates no snippets returned, run again for these to confirm as internet issues could have prevented results.`)
+for(var d of data){
+    if(d.snippets == snippets){
+        console.log("SAME: " + d.id)
+    }
+    if(d.results === 0){
+        console.log("EMPTY: " + d.id);
+    }
+    snippets = d.snippets;
+}
